ac6aef4281dde414e7fed1dbfe7723a12da50e02
Ryan Loh <ryan_loh@meteorgrid.com>
2024-07-29 10:30:28 +0800

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 sudo apt update
-sudo apt install vim snapd zsh zsh-syntax-highlighting sway
-
+sudo apt install vim snapd zsh zsh-syntax-highlighting sway curl
+source /etc/profile
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/dracula/zsh.git
-cd zsh && mv dracula.zsh-theme ~/.oh-my-zsh/themes && mv lib ~/.oh-my-zsh/themes
+mv ~/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes && mv ~/zsh/lib ~/.oh-my-zsh/themes
+rm -rf ~/zsh
 
 git clone -b linux https://github.com/mrchoc/dotfiles.git
 
 cp -r ~/dotfiles/.config/* ~/.config
 cp ~/dotfiles/.zshrc ~
+cp ~/dotfiles/.p10k.zsh ~
+cp ~/dotfiles/.tmux.conf ~
 
-source /etc/profile
 sudo snap install alacritty
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz

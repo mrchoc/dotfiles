215c78bf99bb9c8fcba01ddf1538e63aee002fe5
Ryan Loh <ryanloh28@yahoo.com.sg>
2026-08-06 22:59:16 +0800

    missing treesitter filetype autocmd start

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,31 @@ require("set")
 
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'javascript',
+    'typescript',
+    'tsx',
+    'python',
+    'go',
+    'c',
+    'dart',
+    'lua',
+    'html',
+    'css',
+    'prisma',
+    'java',
+    'rust',
+    'toml',
+    'markdown',
+    'markdown_inline',
+    'terraform',
+    'vimdoc',
+    'yaml',
+    'bash',
+    'vue',
+    'elixir'
+  },
+  callback = function() vim.treesitter.start() end,
+})
